a8b67f6141e8342714bedd0fe7d2b40fed5ac04c
Pavel Liser <liser.pavel@gmail.com>
2022-04-25 18:02:01 +0800

    adding a quote generation button

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from bbquote850.quotes import get_quote
 
-st.write("# Quote of the day 👇")
-st.write(f"### {get_quote()}")
+btn = st.button("Get Quote!")
+
+if btn:
+  st.write("# Quote of the day 👇")
+  st.write(f"### {get_quote()}")
